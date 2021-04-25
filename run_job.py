ba772828f04d3e68034f1c9a31a6d859b9f46f36
Maneesha Perera <maneeshagsp93@gmail.com>
2021-04-26 09:42:15 +1000

    error fixed for 2 gpu

--- run_job.py
+++ run_job.py
@@ -23,7 +23,6 @@ if __name__ == '__main__':
         epochs = 1000
 
         lookback = [1, 3, 7]
-        model_num = 1
 
         for cell_dim in cell_dims:
             for lstm_layer in num_layers:
@@ -31,7 +30,8 @@ if __name__ == '__main__':
                     for lag in lookback:
                         print("current model running")
                         print(cell_dim, lstm_layer, lr, lag)
+                        model_dir = f'{cell_dim}_{lstm_layer}_{lr}_{lag}'
                         run_lstm.run_dnn(file_index, model_name, lstm_layer, cell_dim, epochs, lr, lag, main_dir,
-                                         model_num)
-                        filepath = f'{main_dir}/{model_name}_{model_num}'
+                                         model_dir)
+                        filepath = f'{main_dir}/{model_name}_{model_dir}'
                         process_dnn_output.run_process(filepath, lag)
