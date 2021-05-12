38499bd8986d05ae885bb5fb3199ff623fcdac1f
Maneesha Perera <maneeshagsp93@gmail.com>
2021-05-12 17:46:15 +1000

    combine nn architecture changed

--- src/CNN_architectures/combined_model.py
+++ src/CNN_architectures/combined_model.py
@@ -6,34 +6,36 @@ from tensorflow.keras import layers
 
 def create_network(pc):
     input_layer = keras.Input(shape=(14 * 7, 106), name=f'input_postcode_{pc}')
-    x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=1, name=f'cnn1_postcode_{pc}')(
-        input_layer)
-    x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=2, name=f'cnn2_postcode_ly1_{pc}')(x)
-    x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=4, name=f'cnn3_postcode_ly2_{pc}')(x)
-    x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=8, name=f'cnn3_postcode_ly3_{pc}')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    # x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=1, name=f'cnn1_postcode_{pc}')(
+    #     input_layer)
+    # x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=2, name=f'cnn2_postcode_ly1_{pc}')(x)
+    # x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=4, name=f'cnn3_postcode_ly2_{pc}')(x)
+    # x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=8, name=f'cnn3_postcode_ly3_{pc}')(x)
+    # x = layers.BatchNormalization()(x)
+    # x = layers.Activation("relu")(x)
     # x = layers.Dropout(0.5)(x)
     # x = layers.Flatten(name=f'flatten_postcode_{pc}')(x)
     # x = layers.Dense(14, name=f'dense_postcode_{pc}')(x)
+    x = layers.Dense(14, name=f'dense_postcode_{pc}')(input_layer)
     model = keras.Model(input_layer, x)
     return model
 
 
 def create_grid_network():
     input_grid = keras.Input(shape=(14 * 7, 1), name='input_grid')
-    y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=1, name='cnn1_grid')(input_grid)
+    # y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=1, name='cnn1_grid')(input_grid)
+    # # y = layers.BatchNormalization()(y)
+    # # y = layers.Activation("relu")(y)
+    # # y = layers.Dropout(0.5)(y)
+    # y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=2, name='cnn2_ly1_grid')(y)
+    # y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=4, name='cnn2_ly2_grid')(y)
+    # y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=8, name='cnn2_ly3_grid')(y)
     # y = layers.BatchNormalization()(y)
     # y = layers.Activation("relu")(y)
     # y = layers.Dropout(0.5)(y)
-    y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=2, name='cnn2_ly1_grid')(y)
-    y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=4, name='cnn2_ly2_grid')(y)
-    y = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=8, name='cnn2_ly3_grid')(y)
-    y = layers.BatchNormalization()(y)
-    y = layers.Activation("relu")(y)
-    # y = layers.Dropout(0.5)(y)
     # y = layers.Flatten(name='flatten_grid')(y)
     # y = layers.Dense(14, name='dense_grid')(y)
+    y = layers.Dense(14, name='dense_grid')(input_grid)
     grid = keras.Model(input_grid, y)
     return grid
 
@@ -48,14 +50,14 @@ def create_combine_network():
     pc_6281 = create_network(6281)
     pc_6284 = create_network(6284)
 
-    # grid_network = create_grid_network()
+    grid_network = create_grid_network()
 
-    # combinedInput = layers.concatenate(
-    #     [grid_network.output, pc_6010.output, pc_6014.output, pc_6011.output, pc_6280.output, pc_6281.output,
-    #      pc_6284.output])
     combinedInput = layers.concatenate(
-        [pc_6010.output, pc_6014.output, pc_6011.output, pc_6280.output, pc_6281.output,
+        [grid_network.output, pc_6010.output, pc_6014.output, pc_6011.output, pc_6280.output, pc_6281.output,
          pc_6284.output])
+    # combinedInput = layers.concatenate(
+    #     [pc_6010.output, pc_6014.output, pc_6011.output, pc_6280.output, pc_6281.output,
+    #      pc_6284.output])
     x = layers.LayerNormalization()(combinedInput)
     x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=1)(x)
     x = layers.Conv1D(kernel_size=2, padding='causal', filters=32, dilation_rate=2)(x)
@@ -66,12 +68,12 @@ def create_combine_network():
     # x = layers.Activation("relu")(x)
     x = layers.Flatten(name='flatten_combined')(x)
     x = layers.Dense(14, activation="relu")(x)
-    # hf_model = keras.Model(
-    #     inputs=[grid_network.input, pc_6010.input, pc_6014.input, pc_6011.input, pc_6280.input, pc_6281.input,
-    #             pc_6284.input], outputs=x)
     hf_model = keras.Model(
-        inputs=[pc_6010.input, pc_6014.input, pc_6011.input, pc_6280.input, pc_6281.input,
+        inputs=[grid_network.input, pc_6010.input, pc_6014.input, pc_6011.input, pc_6280.input, pc_6281.input,
                 pc_6284.input], outputs=x)
+    # hf_model = keras.Model(
+    #     inputs=[pc_6010.input, pc_6014.input, pc_6011.input, pc_6280.input, pc_6281.input,
+    #             pc_6284.input], outputs=x)
 
     hf_model.compile(loss=tf.losses.MeanSquaredError(),
                      optimizer=tf.optimizers.Adam(0.0001),
