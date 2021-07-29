11f0406b8b95711d5880a2dcbd3f0b162d9563f3
Maneesha Perera <maneeshagsp93@gmail.com>
2021-07-29 10:34:35 +1000

    error calculation

--- swis_error_calculation.py
+++ swis_error_calculation.py
@@ -48,82 +48,94 @@ def get_grid_error_per_run(grid_model_path, model_path, run, model_name, notcomb
     return level_rmse
 
 
-models = {'0': {'name': 'naive', 'dir': 'benchmark_results/swis_benchmarks', 'runs': 1},
-          '1': {'name': 'arima', 'dir': 'benchmark_results/swis_benchmarks', 'runs': 1},
-          '2': {'name': 'conventional_lstm', 'dir': 'swis_conventional_nn_results', 'runs': 10},
-          '3': {'name': 'SWIS_APPROACH_A', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
-          '4': {'name': 'conventional_cnn', 'dir': 'swis_conventional_nn_results', 'runs': 10},
-          '5': {'name': 'conventional_tcn', 'dir': 'swis_conventional_nn_results', 'runs': 10},
-          '6': {'name': 'SWIS_APPROACH_B_with_clustering', 'dir': 'swis_combined_nn_results/approachB', 'runs': 10},
-          '7': {'name': 'SWIS_APPROACH_B', 'dir': 'swis_combined_nn_results/approachB', 'runs': 10},
-          '8': {'name': 'SWIS_APPROACH_A_SKIP', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
-          '9': {'name': 'SWIS_APPROACH_A_more_layer', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
-          '10': {'name': 'SWIS_APPROACH_A_more_layer_without_norm', 'dir': 'swis_combined_nn_results/approachA',
-                 'runs': 10},
-          '11': {'name': 'SWIS_APPROACH_A_more_layer_with_simple_CNN', 'dir': 'swis_combined_nn_results/approachA',
-                 'runs': 10},
-          '12': {'name': 'SWIS_APPROACH_B_with_clustering2', 'dir': 'swis_combined_nn_results/approachB', 'runs': 10},
-          '13': {'name': 'sequentional_training_approach', 'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
-          '14': {'name': 'SWIS_APPROACH_A_SKIP_GRID_SKIP', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
-          '15': {'name': 'SWIS_APPROACH_A_more_layer_without_norm_grid_skip',
-                 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
-          '16': {'name': 'pc_together_2D_conv_approach',
-                 'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
-          '17': {'name': 'pc_together_2D_conv_approach_with_grid',
-                 'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
-          '18': {'name': 'approachA_increase_number_of_filters',
-                 'dir': 'swis_combined_nn_results/approachA', 'runs': 3},
-          '19': {'name': 'pc_together_2D_conv_approach_with_simple_grid_cnn',
-                 'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
-          '20': {'name': 'SWIS_APPROACH_A_reshape_appraoch',
-                 'dir': 'swis_combined_nn_results/approachA', 'runs': 3},
-          '21': {'name': 'SWIS_APPROACH_B_max_pool',
-                 'dir': 'swis_combined_nn_results/approachB', 'runs': [0, 1, 2, 3, 5, 6, 7, 8, 9]},
-          '22': {'name': 'SWIS_APPROACH_B_with_fully_connected',
-                 'dir': 'swis_combined_nn_results/approachB', 'runs': [1, 2, 3, 4, 5, 6, 7, 9]}
+# models = {'0': {'name': 'naive', 'dir': 'benchmark_results/swis_benchmarks', 'runs': 1},
+#           '1': {'name': 'arima', 'dir': 'benchmark_results/swis_benchmarks', 'runs': 1},
+#           '2': {'name': 'conventional_lstm', 'dir': 'swis_conventional_nn_results', 'runs': 10},
+#           '3': {'name': 'SWIS_APPROACH_A', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
+#           '4': {'name': 'conventional_cnn', 'dir': 'swis_conventional_nn_results', 'runs': 10},
+#           '5': {'name': 'conventional_tcn', 'dir': 'swis_conventional_nn_results', 'runs': 10},
+#           '6': {'name': 'SWIS_APPROACH_B_with_clustering', 'dir': 'swis_combined_nn_results/approachB', 'runs': 10},
+#           '7': {'name': 'SWIS_APPROACH_B', 'dir': 'swis_combined_nn_results/approachB', 'runs': 10},
+#           '8': {'name': 'SWIS_APPROACH_A_SKIP', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
+#           '9': {'name': 'SWIS_APPROACH_A_more_layer', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
+#           '10': {'name': 'SWIS_APPROACH_A_more_layer_without_norm', 'dir': 'swis_combined_nn_results/approachA',
+#                  'runs': 10},
+#           '11': {'name': 'SWIS_APPROACH_A_more_layer_with_simple_CNN', 'dir': 'swis_combined_nn_results/approachA',
+#                  'runs': 10},
+#           '12': {'name': 'SWIS_APPROACH_B_with_clustering2', 'dir': 'swis_combined_nn_results/approachB', 'runs': 10},
+#           '13': {'name': 'sequentional_training_approach', 'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
+#           '14': {'name': 'SWIS_APPROACH_A_SKIP_GRID_SKIP', 'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
+#           '15': {'name': 'SWIS_APPROACH_A_more_layer_without_norm_grid_skip',
+#                  'dir': 'swis_combined_nn_results/approachA', 'runs': 10},
+#           '16': {'name': 'pc_together_2D_conv_approach',
+#                  'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
+#           '17': {'name': 'pc_together_2D_conv_approach_with_grid',
+#                  'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
+#           '18': {'name': 'approachA_increase_number_of_filters',
+#                  'dir': 'swis_combined_nn_results/approachA', 'runs': 3},
+#           '19': {'name': 'pc_together_2D_conv_approach_with_simple_grid_cnn',
+#                  'dir': 'swis_combined_nn_results/approachB', 'runs': 3},
+#           '20': {'name': 'SWIS_APPROACH_A_reshape_appraoch',
+#                  'dir': 'swis_combined_nn_results/approachA', 'runs': 3},
+#           '21': {'name': 'SWIS_APPROACH_B_max_pool',
+#                  'dir': 'swis_combined_nn_results/approachB', 'runs': [0, 1, 2, 3, 5, 6, 7, 8, 9]},
+#           '22': {'name': 'SWIS_APPROACH_B_with_fully_connected',
+#                  'dir': 'swis_combined_nn_results/approachB', 'runs': [1, 2, 3, 4, 5, 6, 7, 9]}
+#           }
+
+models = {'0': {'name': 'swis_parallel_ts', 'dir': 'swis_combined_nn_results/new_models', 'runs': 10},
+          '1': {'name': 'swis_pc_grid_parallel', 'dir': 'swis_combined_nn_results/new_models', 'runs': 10},
+          '2': {'name': 'pc_2d_conv_with_grid_tcn', 'dir': 'swis_combined_nn_results/new_models', 'runs': 10},
           }
 
 stat_models = ['arima', 'naive']
-combined = ['SWIS_APPROACH_A', 'SWIS_APPROACH_B', 'SWIS_APPROACH_B_with_clustering', 'SWIS_APPROACH_A_SKIP',
-            'SWIS_APPROACH_B_with_clustering2', 'SWIS_APPROACH_A_more_layer', 'SWIS_APPROACH_A_more_layer_without_norm',
-            'SWIS_APPROACH_A_more_layer_with_simple_CNN', 'sequentional_training_approach',
-            'SWIS_APPROACH_A_SKIP_GRID_SKIP', 'SWIS_APPROACH_A_more_layer_without_norm_grid_skip',
-            'pc_together_2D_conv_approach', 'pc_together_2D_conv_approach_with_grid'
-    , 'approachA_increase_number_of_filters', 'pc_together_2D_conv_approach_with_simple_grid_cnn',
-            'SWIS_APPROACH_A_reshape_appraoch', 'SWIS_APPROACH_B_max_pool',
-            'SWIS_APPROACH_B_with_fully_connected']
-conventional_nns = ['conventional_lstm', 'conventional_cnn', 'conventional_tcn']
-model_number = sys.argv[1]
-MODEL_NAME = models[model_number]['name']
-PATH = models[model_number]['dir']
-RUN_RANGE = models[model_number]['runs']
+# combined = ['SWIS_APPROACH_A', 'SWIS_APPROACH_B', 'SWIS_APPROACH_B_with_clustering', 'SWIS_APPROACH_A_SKIP',
+#             'SWIS_APPROACH_B_with_clustering2', 'SWIS_APPROACH_A_more_layer', 'SWIS_APPROACH_A_more_layer_without_norm',
+#             'SWIS_APPROACH_A_more_layer_with_simple_CNN', 'sequentional_training_approach',
+#             'SWIS_APPROACH_A_SKIP_GRID_SKIP', 'SWIS_APPROACH_A_more_layer_without_norm_grid_skip',
+#             'pc_together_2D_conv_approach', 'pc_together_2D_conv_approach_with_grid'
+#     , 'approachA_increase_number_of_filters', 'pc_together_2D_conv_approach_with_simple_grid_cnn',
+#             'SWIS_APPROACH_A_reshape_appraoch', 'SWIS_APPROACH_B_max_pool',
+#             'SWIS_APPROACH_B_with_fully_connected']
 
-print(MODEL_NAME)
+combined = ['swis_parallel_ts', 'pc_2d_conv_with_grid_tcn', 'swis_pc_grid_parallel']
+conventional_nns = ['conventional_lstm', 'conventional_cnn', 'conventional_tcn']
+# model_number = sys.argv[1]
+# MODEL_NAME = models[model_number]['name']
+# PATH = models[model_number]['dir']
+# RUN_RANGE = models[model_number]['runs']
+
+#print(MODEL_NAME)
 
 all_errors = []
-dir_path = f'{PATH}/{MODEL_NAME}'
-one_grid_path = f'{PATH}/{MODEL_NAME}'
 
-for RUN in range(0, RUN_RANGE):
-# for RUN in RUN_RANGE:
-    if MODEL_NAME in conventional_nns:
-        one_grid_path = f'{PATH}/{MODEL_NAME}/grid/{RUN}'
-    elif MODEL_NAME in combined:
-        one_grid_path = f'{PATH}/{MODEL_NAME}/{RUN}'
-    notcombined = True
-    if MODEL_NAME in combined:
-        notcombined = False
-    rmse_run_list = get_grid_error_per_run(one_grid_path, dir_path, RUN, MODEL_NAME, notcombined)
-    all_errors.append([rmse_run_list[0], 'grid'])
-    if notcombined:
-        all_errors.append([rmse_run_list[1], 'pc'])
+for model_number in models:
+    MODEL_NAME = models[model_number]['name']
+    PATH = models[model_number]['dir']
+    RUN_RANGE = models[model_number]['runs']
+    print(MODEL_NAME)
+    dir_path = f'{PATH}/{MODEL_NAME}'
+    one_grid_path = f'{PATH}/{MODEL_NAME}'
+    for RUN in range(0, RUN_RANGE):
+        if MODEL_NAME in conventional_nns:
+            one_grid_path = f'{PATH}/{MODEL_NAME}/grid/{RUN}'
+        elif MODEL_NAME in combined:
+            one_grid_path = f'{PATH}/{MODEL_NAME}/{RUN}'
+        notcombined = True
+        if MODEL_NAME in combined:
+            notcombined = False
+        rmse_run_list = get_grid_error_per_run(one_grid_path, dir_path, RUN, MODEL_NAME, notcombined)
+        all_errors.append([MODEL_NAME, rmse_run_list[0], RUN, 'grid'])
+        if notcombined:
+            all_errors.append([MODEL_NAME, rmse_run_list[1], RUN, 'pc'])
 
-all_error_df = pd.DataFrame(all_errors, columns=['NRMSE', 'Level'])
+all_error_df = pd.DataFrame(all_errors, columns=['model_name', 'error', 'run' 'Level'])
 
-error_path = f'{dir_path}/errors/'
-if not os.path.exists(error_path):
-    os.makedirs(error_path)
-all_error_df.to_csv(f'{error_path}/final_errors.csv')
+error_path = f'swis_combined_nn_results/all_errors.csv'
+all_error_df.to_csv(error_path)
+# if not os.path.exists(error_path):
+#     os.makedirs(error_path)
+# all_error_df.to_csv(f'{error_path}/final_errors.csv')
 
 # mean_std_df = all_error_df.groupby(by='Level').agg({'NRMSE': ['mean', 'std']})
 # mean_err = mean_std_df.values[0][0]
